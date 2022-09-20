e6fe6e7d6a612b24bbde7f060cce7963c902cd52
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2022-09-20 11:12:39 +0500

    secret key settings

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+
+# Add .env variables anywhere before SECRET_KEY
+
+    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ul38(&pon1)tpvhk(w_d-c6(z7z)hp6w7@#y=*+^rnhl_xqzah'
+# UPDATE secret key
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
